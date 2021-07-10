a6720dcd9ffeae3cd0b5e1674c104cc82d69d66d
Ali Shahwali <ali_zinq@hotmail.com>
2021-07-10 20:31:14 +0200

    heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
 config :northwind_api, NorthwindApiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "warm-bayou-39031.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
